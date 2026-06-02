1c393b0d142c10a9ee368724dc20cefc9c3b0dd3
Tzouela Kontrasi <tzoela23@icloud.com>
2026-06-02 15:42:03 +0200

    Add citizen functions

---
+++ practice.ts
@@ -0,0 +1,42 @@
+interface Citizen {
+  id: number;
+  name: string;
+  age: number;
+  hasJob: boolean;
+}
+
+function getUnemployedAdults (citizens: Citizen[]): Citizen[] {
+  return citizens.filter((citizen) => citizen.age >= 18 && citizen.hasJob === false);
+}
+
+console.log(getUnemployedAdults([
+  {id: 1, name: "Sara", age: 18, hasJob: false},
+  {id: 2, name: "Ben", age: 19, hasJob: false},
+  {id: 3, name: "Tom", age: 19, hasJob: true},
+  {id: 4, name: "Jane", age: 17, hasJob: false}
+]));
+
+function getAverageAge(citizens: Citizen[]): number {
+  const initialValue = 0;
+  const sumAges = citizens.reduce((sum, currentValue) => sum + currentValue.age, initialValue);
+  const averageAge = sumAges / citizens.length;
+  return averageAge;
+}
+
+console.log(getAverageAge([
+  {id: 1, name: "Sara", age: 18, hasJob: false},
+  {id: 2, name: "Ben", age: 19, hasJob: false},
+  {id: 3, name: "Tom", age: 19, hasJob: true},
+  {id: 4, name: "Jane", age: 17, hasJob: false}
+]));
+
+function sortByAge (citizens: Citizen[]): Citizen[] {
+  return citizens.toSorted((a, b) => a.age - b.age);
+}
+
+console.log(sortByAge([
+  {id: 1, name: "Sara", age: 18, hasJob: false},
+  {id: 2, name: "Ben", age: 19, hasJob: false},
+  {id: 3, name: "Tom", age: 19, hasJob: true},
+  {id: 4, name: "Jane", age: 17, hasJob: false}
+]));
