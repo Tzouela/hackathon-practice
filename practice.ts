35d00df1fc4f3ce7673fb289999996b6a8419126
Tzouela Kontrasi <tzoela23@icloud.com>
2026-06-02 22:56:27 +0200

    Add JSON parsing practice

--- practice.ts
+++ practice.ts
@@ -1,42 +1,68 @@
-interface Citizen {
+// interface Citizen {
+//   id: number;
+//   name: string;
+//   age: number;
+//   hasJob: boolean;
+// }
+
+// function getUnemployedAdults (citizens: Citizen[]): Citizen[] {
+//   return citizens.filter((citizen) => citizen.age >= 18 && citizen.hasJob === false);
+// }
+
+// console.log(getUnemployedAdults([
+//   {id: 1, name: "Sara", age: 18, hasJob: false},
+//   {id: 2, name: "Ben", age: 19, hasJob: false},
+//   {id: 3, name: "Tom", age: 19, hasJob: true},
+//   {id: 4, name: "Jane", age: 17, hasJob: false}
+// ]));
+
+// function getAverageAge(citizens: Citizen[]): number {
+//   const initialValue = 0;
+//   const sumAges = citizens.reduce((sum, currentValue) => sum + currentValue.age, initialValue);
+//   const averageAge = sumAges / citizens.length;
+//   return averageAge;
+// }
+
+// console.log(getAverageAge([
+//   {id: 1, name: "Sara", age: 18, hasJob: false},
+//   {id: 2, name: "Ben", age: 19, hasJob: false},
+//   {id: 3, name: "Tom", age: 19, hasJob: true},
+//   {id: 4, name: "Jane", age: 17, hasJob: false}
+// ]));
+
+// function sortByAge (citizens: Citizen[]): Citizen[] {
+//   return citizens.toSorted((a, b) => a.age - b.age);
+// }
+
+// console.log(sortByAge([
+//   {id: 1, name: "Sara", age: 18, hasJob: false},
+//   {id: 2, name: "Ben", age: 19, hasJob: false},
+//   {id: 3, name: "Tom", age: 19, hasJob: true},
+//   {id: 4, name: "Jane", age: 17, hasJob: false}
+// ]));
+
+interface Employee {
   id: number;
   name: string;
   age: number;
   hasJob: boolean;
 }
 
-function getUnemployedAdults (citizens: Citizen[]): Citizen[] {
-  return citizens.filter((citizen) => citizen.age >= 18 && citizen.hasJob === false);
-}
+const jsonData = `[
+  {"id": 1, "name": "Sara", "age": 18, "hasJob": false},
+  {"id": 2, "name": "Ben", "age": 19, "hasJob": false},
+  {"id": 3, "name": "Tom", "age": 25, "hasJob": true},
+  {"id": 4, "name": "Jane", "age": 17, "hasJob": false}
+]`;
 
-console.log(getUnemployedAdults([
-  {id: 1, name: "Sara", age: 18, hasJob: false},
-  {id: 2, name: "Ben", age: 19, hasJob: false},
-  {id: 3, name: "Tom", age: 19, hasJob: true},
-  {id: 4, name: "Jane", age: 17, hasJob: false}
-]));
+const employeesArray: Employee[] = JSON.parse(jsonData);
 
-function getAverageAge(citizens: Citizen[]): number {
-  const initialValue = 0;
-  const sumAges = citizens.reduce((sum, currentValue) => sum + currentValue.age, initialValue);
-  const averageAge = sumAges / citizens.length;
-  return averageAge;
-}
+console.log(employeesArray);
 
-console.log(getAverageAge([
-  {id: 1, name: "Sara", age: 18, hasJob: false},
-  {id: 2, name: "Ben", age: 19, hasJob: false},
-  {id: 3, name: "Tom", age: 19, hasJob: true},
-  {id: 4, name: "Jane", age: 17, hasJob: false}
-]));
+const onlyUnemployed = employeesArray.filter((employee) => employee.age >= 18 && employee.hasJob === false);
 
-function sortByAge (citizens: Citizen[]): Citizen[] {
-  return citizens.toSorted((a, b) => a.age - b.age);
-}
+console.log(onlyUnemployed);
 
-console.log(sortByAge([
-  {id: 1, name: "Sara", age: 18, hasJob: false},
-  {id: 2, name: "Ben", age: 19, hasJob: false},
-  {id: 3, name: "Tom", age: 19, hasJob: true},
-  {id: 4, name: "Jane", age: 17, hasJob: false}
-]));
+const convertedData = JSON.stringify(onlyUnemployed);
+
+console.log(convertedData);
